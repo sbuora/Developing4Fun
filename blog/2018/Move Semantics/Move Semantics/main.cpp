2e25aef57ebdad5be0e3ccbb3c60933d4bbbe110
Stefano Buora <info@buora.it>
2018-01-17 18:11:32 +0100

    First commit of a console project from MAC

---
+++ blog/2018/Move Semantics/Move Semantics/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Move Semantics
+//
+//  Created by Stefano on 17/01/2018.
+//  Copyright © 2018 Developing4Fun. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
